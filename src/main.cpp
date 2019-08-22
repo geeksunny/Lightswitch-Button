9635952654d4139ca1e2b1c35ec94fa0ad34ec29
Justin Swanson <geeksunny@gmail.com>
2019-08-22 04:16:35 -0500

    Serial console fix

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ bool modeSwitchActivated() {
 void setup() {
 #ifdef DEBUG_MODE
   Serial.begin(BAUD_RATE);
+  delay(3000);  // Give serial console a chance to get ready
+  Serial.println("Serial console ready.");
 #endif
 
   pinMode(MODE_SWITCH_PIN, INPUT_PULLUP);
